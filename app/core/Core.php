bbf4aede7464b36e112467a8c333698a11011086
LuisHenrique <81261202+Luyzhz@users.noreply.github.com>
2021-04-27 15:05:30 -0300

    Update Core.php

--- app/core/Core.php
+++ app/core/Core.php
@@ -3,6 +3,11 @@
     class Core
     {
         private $url;
+
+        private $controller;
+        private $method;
+        private $params = array();
+
         public function __construct()
         {
             
@@ -10,7 +15,22 @@
 
         public function start($request)
         {
-            var_dump($request);
-            //$this->url = 
+            if ($request['url']){
+
+            $this->url = explode('/', $request['url']);
+
+            $this->controller = ucfirst($this->url[0]).'Controller';
+            array_shift($this->url);
+
+            $this->method = $this->url[0];
+            array_shift($this->url);
+
+            $this->params = $this->url;
+            } else {
+                $this->controller = 'LoginController';
+                $this->method = 'index';
+            }
+            call_user_func(array(new $this->controller, $this->method), $this->params);
+            var_dump($this->controller, $this->method, $this->params);
         }
     }
